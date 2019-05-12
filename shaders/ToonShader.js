cd9d40c9e299ccdbc72caef4c6773c10cf3b6f62
Somya Arora <arorasomya64@gmail.com>
2019-05-12 14:15:57 -0400

    adding toon effect

---
+++ shaders/ToonShader.js
@@ -0,0 +1,54 @@
+/**
+ * inspired from:
+ * https://prideout.net/blog/old/blog/index.html@p=22.html
+ */
+
+THREE.SepiaShader = {
+
+	uniforms: {
+
+		"tDiffuse": { value: null },
+        "amount":   { value: 1.0 },
+        "mat4 Projection": {value: null},
+        "mat4 Modelview": {value: null},
+        "mat3 NormalMatrix": {value: null},
+        "vec3 DiffuseMaterial": {value: null}
+	},
+
+	vertexShader: [
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vUv = uv;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform float amount;",
+
+		"uniform sampler2D tDiffuse;",
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vec4 color = texture2D( tDiffuse, vUv );",
+			"vec3 c = color.rgb;",
+
+			"color.r = dot( c, vec3( 1.0 - 0.607 * amount, 0.769 * amount, 0.189 * amount ) );",
+			"color.g = dot( c, vec3( 0.349 * amount, 1.0 - 0.314 * amount, 0.168 * amount ) );",
+			"color.b = dot( c, vec3( 0.272 * amount, 0.534 * amount, 1.0 - 0.869 * amount ) );",
+
+			"gl_FragColor = vec4( min( vec3( 1.0 ), color.rgb ), color.a );",
+
+		"}"
+
+	].join( "\n" )
+
+};
